d5b9a4a2f5c3bd957947c13873a9c13544161f52
nambui <nambui906@gmail.com>
2023-05-19 11:26:42 +0700

    update code

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -2,11 +2,36 @@ resource "google_container_cluster" "primary" {
   name               = var.config.name
   location           = var.config.location
   initial_node_count = var.config.number_node
+  pod_security_policy_config {
+    enabled = true
+  }
+  master_auth {
+    client_certificate_config {
+      issue_client_certificate = false
+    }
+  }
+  private_cluster_config  {
+    enable_private_nodes = true
+    enable_private_endpoint = true
+  }
+  workload_metadata_config {
+    node_metadata = "GKE_METADATA"
+  }
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = "10.0.0.0/8"
+  }
+  resource_labels = "test"
   node_config {
     service_account = var.config
+    labels = {
+      propose = "test"
+    } 
     oauth_scopes = [
-     
+     var.oauth_scopes
     ]
     tags = ["foo", "bar"]
+  }
+  network_policy {
+    enabled = true
   }
 }
