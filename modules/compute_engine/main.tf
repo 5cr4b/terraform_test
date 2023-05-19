8c52751e8fd2144657633d743c5a18632cb5a6e4
nambui <nambui906@gmail.com>
2023-05-19 16:35:27 +0700

    update code

--- modules/compute_engine/main.tf
+++ modules/compute_engine/main.tf
@@ -1,8 +1,8 @@
 resource "google_compute_instance" "gcp_compute_instance" {
   name          = "${var.config.name}-${var.config.customer}-${var.config.id}"
-  project       = ""
-  machine_type  = ""
-  zone          = ""
+  project       = var.config.project
+  machine_type  = var.config.machine_type
+  zone          = var.config.zone
   tags          = ["",""]
 
   boot_disk {
