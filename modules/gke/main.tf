d51e30d20dc1148d73117cb08523354b29ea72f6
nambui <nambui906@gmail.com>
2023-05-19 13:39:55 +0700

    update code

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -2,6 +2,8 @@ resource "google_container_cluster" "primary" {
   name               = var.config.name
   location           = var.config.location
   initial_node_count = var.config.number_node
+  #resource_labels = "test"
+
   pod_security_policy_config {
     enabled = true
   }
@@ -14,13 +16,13 @@ resource "google_container_cluster" "primary" {
     enable_private_nodes = true
     enable_private_endpoint = true
   }
-  workload_metadata_config {
-    node_metadata = "GKE_METADATA"
-  }
+  #workload_metadata_config {
+  #  node_metadata = "GKE_METADATA"
+  #}
   ip_allocation_policy {
     cluster_ipv4_cidr_block = "10.0.0.0/8"
   }
-  resource_labels = "test"
+  
   node_config {
     service_account = var.config.service_account
     labels = {
--- modules/network_vpc/main.tf
+++ modules/network_vpc/main.tf
@@ -3,7 +3,7 @@ resource "google_compute_network" "gcp_vpc_network" {
   name                      =  "${var.config.name}-${var.config.customer}-${var.config.id}"
   description               =  "vpc network"
   auto_create_subnetworks   = false
-  mtu                       =  "1460" // default Maximum Transmission Unit in bytes
+  #mtu                       =  "1460" // default Maximum Transmission Unit in bytes
 }
 
 
